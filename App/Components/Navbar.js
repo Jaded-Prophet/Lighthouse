b6b69de8dad078415f3b83390f584c63ec9ea2b8
byeo630 <byeo630@gmail.com>
2016-04-08 15:21:40 -0700

    (feat) Make tab bar have two tabs

--- App/Components/Navbar.js
+++ App/Components/Navbar.js
@@ -28,23 +28,15 @@ class TabBar extends React.Component {
         <TabBarIOS.Item
           selected={this.state.selectedTab === 'tabOne'}
           onPress={() => this.setTab('tabOne')}
-          systemIcon="history">
+          systemIcon="contacts">
           <Friends />
         </TabBarIOS.Item>
         <TabBarIOS.Item
           selected={this.state.selectedTab === 'tabTwo'}
           onPress={() => this.setTab('tabTwo')}
-          systemIcon="bookmarks">
+          systemIcon="favorites">
             <View style={styles.tabContent}>
               <Text style={styles.tabText}>Tab Two</Text>
-            </View>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          selected={this.state.selectedTab === 'tabThree'}
-          onPress={() => this.setTab('tabThree')}
-          systemIcon="more">
-            <View style={styles.tabContent}>
-              <Text style={styles.tabText}>Tab Three</Text>
             </View>
         </TabBarIOS.Item>
       </TabBarIOS>
