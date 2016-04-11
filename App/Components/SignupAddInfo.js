10aa9d4b7fc53020ea9dd9f3b7d48fc8df0a2c75
Tor Sinclair <tor@romanesco.se>
2016-04-11 10:50:14 -0700

    Add styles to SignupAddInfo.js

--- App/Components/SignupAddInfo.js
+++ App/Components/SignupAddInfo.js
@@ -1,35 +1,68 @@
-import React, {
-  Text,
+var React = require('react-native');
+
+var {
   View,
-  Image,
+  Text,
   StyleSheet,
-  TouchableHiglight
-} from 'react-native';
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicatorIOS
+} = React;
 
-var styles = StyleSheet.create ({
-  container: {
-    flex: 1
-  },
-  buttonText: {
-    color: 'black',
-    alignSelf: 'center',
-    fontSize: 14
-  },
-  text: {
-    color: 'black',
-    fontSize: 16
-  }
-});
-
-class SignupAddInfo extends React.Component {
-
-  render() {
+class SignupAddInfo extends React.Component{
+  render(){
+    console.log(this.props);
     return (
-      <View>
+      <View style={styles.container}>
+        <Text style={styles.signupWelcome}>Welcome</Text>
       </View>
     )
   }
-
 }
+
+// stylesheet
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor:'#48BBEC'
+  },
+  title: {
+    marginBottom: 25,
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  searchInput: {
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 30
+  },
+  signupWelcom: {
+    fontSize: 50
+  }
+});
 
 module.exports = SignupAddInfo;
