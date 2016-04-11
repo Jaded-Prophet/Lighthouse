57a0845153ee0f3a26328ea04571080dc24c388a
Krista Moroder <krista@krista.local>
2016-04-11 11:27:19 -0700

    (Feat) Adds user properties to profile page, includes styling

--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -3,32 +3,68 @@ import React, {
   Text,
   Image,
   StyleSheet,
-  Component
+  Component,
+  ScrollView
 } from 'react-native';
 
 class Profile extends Component{
+  
+  getRowTitle(user, item) {
+    item = item;
+    return item[0] ? item[0].toUpperCase() + item.slice(1) : item;
+  }
+
+
   render(){
+  console.log('props are', this.props);
+
+    var userInfo = this.props.userInfo.password;
+    // NOTE: replace topic array with new user info
+    var topicArr = ['email', 'profileImageURL'];
+    
+    var list = topicArr.map((item, index) => {
+      if(!userInfo[item]) {
+        return
+          <View key={index} />
+      } else {
+        return (
+          <View key={index}>
+            <View style={styles.rowContainer}>
+              <Text style={styles.rowTitle}> {this.getRowTitle(userInfo, item)} </Text>
+              <Text style={styles.rowContent}> {userInfo[item]} </Text>
+              <View style = {styles.separator} />
+            </View>
+          </View>
+        )
+      }
+    })
+
     return (
-      <View style={styles.container}>
+      <View>
         <View style={styles.badgeContainer}>
-          <Image style={styles.badgeImage} source={{uri: 'https://avatars0.githubusercontent.com/u/99825?v=3&s=460'}} />
-        <Text style={styles.badgeName}> "Fred"</Text>
+            <Image style={styles.badgeImage} source={{uri: this.props.userInfo.password.profileImageURL}} />
+          <Text style={styles.badgeName}> {this.props.userInfo.password.email}</Text>
+        </View>
+        <View style={styles.container}>
+          {list}
+        </View>
       </View>
-      <View style={styles.container}>
-        <Text style={styles.hello}>On Profile Page</Text>
-      </View>
-    </View>
     )
   }
 }
 
 var styles = {
   container: {
-    marginTop: 55
+    flex: 1,
+    width: 300,
+    marginLeft: 20,
+    marginTop: 10
   },
   badgeContainer: {
     backgroundColor: '#48BBEC',
-    paddingBottom: 10
+    paddingBottom: 10,
+    marginTop: 55,
+    width: 400
   },
   badgeName: {
     alignSelf: 'center',
@@ -37,20 +73,31 @@ var styles = {
     marginBottom: 5,
     color: 'white'
   },
-  badgeHandle: {
-    alignSelf: 'center',
-    fontSize: 16,
-    color: 'white'
-  },
   badgeImage: {
-    height: 200,
-    width: 200,
-    borderRadius: 65, 
-    marginTop: 10,
+    height: 126,
+    width: 126,
+    borderRadius: 63, 
+    marginTop: 20,
     alignSelf: 'center'
+  },
+  rowContainer: {
+    padding: 10
+  },
+  rowTitle: {
+    color: '#48BBEC',
+    fontSize: 16
+  },
+  rowContent: {
+    fontSize: 19
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#E4E4E4',
+    flex: 1,
+    marginLeft: 15,
+    marginTop: 5
   }
 };
 
 module.exports = Profile;
-
 
