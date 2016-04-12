7252a7c245e76f413a08a5c2b0eb1d9bb00b43c3
Steven Tran <steventran06@gmail.com>
2016-04-12 00:54:35 -0700

    (feat) Add user location and destination marker

--- App/Components/Map.js
+++ App/Components/Map.js
@@ -1,17 +1,70 @@
 var React = require('react-native');
+var api = require('../Utils/api');
 
 var {
   View,
-  WebView,
-  StyleSheet
+  Text,
+  MapView,
+  StyleSheet,
+  TouchableHighlight
 } = React;
 
 class Map extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      position: null,
+      destLat: 45,
+      destLong: -122.4194
+    };
+  }
+
+  getLocation() {
+    var that = this;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      that.setState({
+        position: position,
+        destLat: 37.7749,
+        destLong: -122.4194
+      });
+      console.log(that.props.userInfo);
+      api.setUserLocation(that.props.userInfo, position);
+      console.log(that.state.position);
+    });
+  }
 
   render() {
+    var markers = [
+      {
+        latitude: 37.7749,
+        longitude: -122.4194,
+        title: 'Middle of San Francisco',
+        subtitle: '1234 Foo Drive'
+      }
+    ];
+
+    var region = {
+      latitude: 37.7749,
+      longitude: -122.4194,
+      latitudeDelta: 0.125,
+      longitudeDelta: 0.125
+    };
+
     return (
       <View style={styles.container} >
-        <WebView url='http://www.google.com/maps' />
+        <MapView
+          style={styles.map}
+          region={region}
+          annotations={markers}
+          showsUserLocation={true}
+          // followUserLocation={true}
+          showPointsOfInterest={true}
+          mapType='satellite' />
+        <TouchableHighlight
+          onPress={this.getLocation.bind(this)}>
+          <Text>BUTTON</Text>
+        </TouchableHighlight>
+
       </View>
     )
   }
@@ -28,6 +81,10 @@ var styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F6F6EF',
     flexDirection: 'column'
+  },
+  map: {
+    width: 350,
+    height: 500
   }
 });
 
