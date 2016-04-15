82f6b78081eafe354b28562c1f1959e160349f17
Steven Tran <steventran06@gmail.com>
2016-04-15 15:10:59 -0700

    feat/Create api helpers for search friends by email/search groups by name

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -121,6 +121,48 @@ var api = {
         // Passing in the this.getUserData since the this binding is lost inside of the async function
         return getFriendData(this.getUserData);
       });
+  },
+
+  findUserByEmail(emailInput) {
+    var users = 'https://project-sapphire.firebaseio.com/UserData.json';
+    return fetch(users)
+      .then((res) => res.json())
+      .then((users) => {
+        async function searchFriendData(callback) {
+          var results = [];
+          for (k in users) {
+            if (users[k].email) {
+              if (users[k].email.toLowerCase().includes(emailInput.toLowerCase())) {
+                console.log(users[k])
+                await callback(users[k]).then((res) => {
+                  res.uid = users[k];
+                  results.push(res);
+                });
+              }
+            }
+          }
+          return results;
+        };
+        return searchFriendData(this.getUserData);
+
+      });
+  },
+
+  findGroupByName(nameInput) {
+    var groups = 'https://project-sapphire.firebaseio.com/Groups.json';
+    return fetch(groups)
+      .then(res => res.json())
+      .then((groups) => {
+        var results = [];
+        for (k in groups) {
+          if (k.toLowerCase().includes(nameInput.toLowerCase())) {
+            groups[k].groupName = k
+            results.push(groups[k]);
+          }
+        };
+        return results;
+      })
+
   }
 };
 
