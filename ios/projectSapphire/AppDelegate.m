128b84bf99b872be41bc578c2052773028ae49b6
Tor Sinclair <tor@romanesco.se>
2016-04-18 00:08:17 -0700

    (feature) add fastlane configuration file

--- ios/projectSapphire/AppDelegate.m
+++ ios/projectSapphire/AppDelegate.m
@@ -31,7 +31,14 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://10.0.0.145:8081/index.ios.bundle?platform=ios&dev=true"];
+#ifdef DEBUG
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
+  
+  
 
   /**
    * OPTION 2
