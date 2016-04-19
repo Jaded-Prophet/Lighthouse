79ce04c7559e238a02eaf2d1353bc18a502dff86
byeo630 <byeo630@gmail.com>
2016-04-19 11:36:20 -0700

    (doc) Add documentation for the server.js file

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,19 @@
 var bodyParser = require('body-parser');
-// var http = require('http').createServer(app);
-// var io = require('socket.io').listen(http);
 
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-// var http = require('http');
-// var server = http.createServer(app);
 
 console.log('Sapphire is listening in on 4568');
 server.listen(4568);
 
 app.use(bodyParser());
+
+// Most of these pages are for the webapp version. The link is here => http://107.170.3.84:4568/map
+// if http://107.170.3.84:4568/map is down, make sure that the digital ocean server is running.
+// Entering `ssh root@107.170.3.84` in the terminal will connect you to the digital ocean server.
+// Once you're in the server, navigate into the projectSapphire/Server folder in the root, then run
+// `nodemon server.js` to run your server.
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -47,6 +49,7 @@ app.post('/connect',
     res.status(200).send('I think it worked');
   });
 
+// Here we write the code for socket.io
 io.on('connection', function(socket) {
   socket.on('chat message', (msg) => {
     console.log('Something Happened!', msg);
